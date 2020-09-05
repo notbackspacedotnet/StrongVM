237cb5c8f4e0c382ea20b313f7e0f1e76e31b7d2
Backspace <70749515+notbackspacedotnet@users.noreply.github.com>
2020-09-05 22:29:38 +0700

    Add files via upload

---
+++ Example/C#/Custom Rename/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Custom.Rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSecretMessage();
+            WriteSecretMessage2();
+        }
+
+        /// <summary>
+        /// Custom Rename Attribute Example
+        /// Replace 'RelaceMe' With New Method Name
+        /// </summary>
+        /// <remarks></remarks>
+        [Obfuscation(Feature = "rename=RelaceMe", Exclude = false)]
+        public static void WriteSecretMessage()
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(Convert.FromBase64String("SGVsbG8gV29ybGQ=")));
+        }
+
+        /// <summary>
+        ///  Custom Rename Attribute Example 2
+        ///  New Method Name '44BDBFBFD'
+        ///  </summary>
+        /// <remarks></remarks>
+        [Obfuscation(Feature = "rename=44BDBFBFD", Exclude = false)]
+        public static void WriteSecretMessage2()
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(Convert.FromBase64String("SGVsbG8gV29ybGQ=")));
+        }
+    }
+}
---
+++ Example/VB.Net/Custom Rename/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Reflection
+
+Module Module1
+
+    Sub Main()
+        WriteSecretMessage()
+        WriteSecretMessage2()
+    End Sub
+
+    ''' <summary>
+    ''' Custom Rename Attribute Example
+    ''' Replace 'RelaceMe' With New Method Name
+    ''' </summary>
+    ''' <remarks></remarks>
+    <Obfuscation(Feature:="rename=RelaceMe", Exclude:=False)>
+    Sub WriteSecretMessage()
+        Console.WriteLine(Text.Encoding.ASCII.GetString(Convert.FromBase64String("SGVsbG8gV29ybGQ=")))
+    End Sub
+
+    ''' <summary>
+    ''' Custom Rename Attribute Example 2
+    ''' New Method Name '44BDBFBFD'
+    ''' </summary>
+    ''' <remarks></remarks>
+    <Obfuscation(Feature:="rename=44BDBFBFD", Exclude:=False)>
+    Sub WriteSecretMessage2()
+        Console.WriteLine(Text.Encoding.ASCII.GetString(Convert.FromBase64String("SGVsbG8gV29ybGQ=")))
+    End Sub
+
+End Module
